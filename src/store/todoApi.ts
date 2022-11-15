a89cccd0d3bc886c989c355485dfff958d79f2d9
Daniil Antipov <a1@1s-MacBook-Pro.local>
2022-11-15 18:07:43 +0300

    todo Feature: добавлен попап для изменения текста, добалвено и стилизовано логика выполненного задания

--- src/store/todoApi.ts
+++ src/store/todoApi.ts
@@ -10,7 +10,7 @@ type PostResponce = Post[];
 
 export const todoApi = createApi({
   reducerPath: 'todoApi',
-  baseQuery: fetchBaseQuery({baseUrl: BASE_URL}),
+  baseQuery: fetchBaseQuery({baseUrl: GITHUB_URL}),
   tagTypes: ['Todos'],
   endpoints: (builder) => ({
     getTodo: builder.query({
