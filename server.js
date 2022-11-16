00d11e326d820fe95448a6af70829426e94047cc
Daniil Antipov <a1@1s-MacBook-Pro.local>
2022-11-16 10:51:45 +0300

    todo Feature: deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('.public/mocks/db.json');
+const router = jsonServer.router('./public/mocks/db.json');
 const middlewares = jsonServer.defaults({
   static: './build'
 });
