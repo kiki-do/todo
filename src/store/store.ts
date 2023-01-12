d0e0c5f76d61a2866eeda0aafc7aded4599333d8
kiki-do <74352957+kiki-do@users.noreply.github.com>
2023-01-12 15:44:44 +0300

    Done

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,32 +1,13 @@
 import dataReducer from "./dataSlice/slice";
-import { AnyAction, combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "redux-persist";
-import hardSet from "redux-persist/es/stateReconciler/hardSet";
-import storage from "redux-persist/es/storage";
-
-export const rootReducer = combineReducers({
-	data: dataReducer,
-});
-
-export type RootReducer = ReturnType<typeof rootReducer>;
-
-// const persistedReducer = persistReducer(
-// 	persistConfig,
-// 	rootReducer
-// ) as typeof rootReducer;
+import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
 	reducer: {
 		data: dataReducer,
 	},
-	middleware: getDefaultMiddleware =>
-		getDefaultMiddleware({
-			serializableCheck: false,
-		}).concat([]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export const persistor = persistStore(store);
 export default store;
